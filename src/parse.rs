eedf59d44f6d94fd759526c57af1c61a5aa14ae7
cunitac <cunitac@gmail.com>
2021-06-22 20:41:16 +0900

    IResult を見やすく

--- src/parse.rs
+++ src/parse.rs
@@ -7,8 +7,10 @@ use nom::combinator::{all_consuming, map, map_res, opt, recognize, value};
 use nom::error::ParseError;
 use nom::multi::{fold_many0, many0, separated_list0, separated_list1};
 use nom::sequence::{delimited, pair, preceded, tuple};
-use nom::{IResult, Parser};
+use nom::Parser;
 use nom_locate::position;
+
+type IResult<'a, O> = nom::IResult<Span<'a>, O>;
 
 use ast::{
     ASTree, Block, Declarations, DefinedFunction, DefinedVariable, Expr, Identifier, Literal,
@@ -17,13 +19,13 @@ use ast::{
 
 type Span<'a> = nom_locate::LocatedSpan<&'a str>;
 
-pub fn parse(source: &'static str) -> Result<ASTree, anyhow::Error> {
+pub fn parse(source: &'static str) -> anyhow::Result<ASTree> {
     let source = Span::new(source);
     let (_, ast) = all_consuming(ast)(source)?;
     Ok(ast)
 }
 
-fn ast(source: Span) -> IResult<Span, ASTree> {
+fn ast(source: Span) -> IResult<ASTree> {
     let (source, position) = position(source)?;
     let (source, declarations) = declarations(source)?;
     let (source, _skip) = skip(source)?;
@@ -36,7 +38,7 @@ fn ast(source: Span) -> IResult<Span, ASTree> {
     ))
 }
 
-fn declarations(source: Span) -> IResult<Span, Declarations> {
+fn declarations(source: Span) -> IResult<Declarations> {
     let mut ret = Declarations::default();
     enum Declaration<'a> {
         Defun(DefinedFunction<'a>),
@@ -52,7 +54,7 @@ fn declarations(source: Span) -> IResult<Span, Declarations> {
     Ok((source, ret))
 }
 
-fn defun(source: Span) -> IResult<Span, DefinedFunction> {
+fn defun(source: Span) -> IResult<DefinedFunction> {
     let (source, ret_type) = type_ref(source)?;
     let (source, name) = s(identifier)(source)?;
     let params = separated_list0(s(tag(",")), s(param));
@@ -69,16 +71,14 @@ fn defun(source: Span) -> IResult<Span, DefinedFunction> {
     ))
 }
 
-fn defvars(source: Span) -> IResult<Span, Vec<DefinedVariable>> {
+fn defvars(source: Span) -> IResult<Vec<DefinedVariable>> {
     let (source, r#type) = type_ref(source)?;
     let (source, vars) = separated_list1(s(tag(",")), s(defvar_type(r#type)))(source)?;
     let (source, _semicolon) = s(tag(";"))(source)?;
     Ok((source, vars))
 }
 
-fn defvar_type<'a>(
-    r#type: TypeRef<'a>,
-) -> impl FnMut(Span<'a>) -> IResult<Span<'a>, DefinedVariable<'a>> {
+fn defvar_type<'a>(r#type: TypeRef<'a>) -> impl FnMut(Span<'a>) -> IResult<DefinedVariable<'a>> {
     move |source| {
         let (source, name) = identifier(source)?;
         Ok((
@@ -91,27 +91,27 @@ fn defvar_type<'a>(
     }
 }
 
-fn type_ref(source: Span) -> IResult<Span, TypeRef> {
+fn type_ref(source: Span) -> IResult<TypeRef> {
     let (source, name) = identifier(source)?;
     Ok((source, TypeRef { name }))
 }
 
-fn param(source: Span) -> IResult<Span, Param> {
+fn param(source: Span) -> IResult<Param> {
     let (source, r#type) = identifier(source)?;
     let (source, name) = s(identifier)(source)?;
     Ok((source, Param::new(r#type, name)))
 }
 
-fn block(source: Span) -> IResult<Span, Block> {
+fn block(source: Span) -> IResult<Block> {
     let (source, stmts) = delimited(tag("{"), many0(s(stmt)), s(tag("}")))(source)?;
     Ok((source, Block { stmts }))
 }
 
-fn stmt(source: Span) -> IResult<Span, Stmt> {
+fn stmt(source: Span) -> IResult<Stmt> {
     alt((expr_stmt, map(block, Stmt::Block), r#if))(source)
 }
 
-fn r#if(source: Span) -> IResult<Span, Stmt> {
+fn r#if(source: Span) -> IResult<Stmt> {
     let (source, _if) = tag("if")(source)?;
     let (source, cond) = paren(expr)(source)?;
     let (source, then) = s(stmt)(source)?;
@@ -119,17 +119,17 @@ fn r#if(source: Span) -> IResult<Span, Stmt> {
     Ok((source, Stmt::r#if(cond, then, r#else)))
 }
 
-fn expr_stmt(source: Span) -> IResult<Span, Stmt> {
+fn expr_stmt(source: Span) -> IResult<Stmt> {
     let (source, expr) = expr(source)?;
     let (source, _) = s(tag(";"))(source)?;
     Ok((source, Stmt::Expr(expr)))
 }
 
-fn expr(source: Span) -> IResult<Span, Expr> {
+fn expr(source: Span) -> IResult<Expr> {
     expr10(source)
 }
 
-fn expr10(source: Span) -> IResult<Span, Expr> {
+fn expr10(source: Span) -> IResult<Expr> {
     let (source, init) = expr9(source)?;
     let (source, qtce) = opt(tuple((s(char('?')), s(expr), s(char(':')), s(expr10))))(source)?;
     Ok((
@@ -142,17 +142,17 @@ fn expr10(source: Span) -> IResult<Span, Expr> {
     ))
 }
 
-fn expr9(source: Span) -> IResult<Span, Expr> {
+fn expr9(source: Span) -> IResult<Expr> {
     let (source, init) = expr8(source)?;
     fold_many0(preceded(s(tag("||")), s(expr8)), init, Expr::logical_or)(source)
 }
 
-fn expr8(source: Span) -> IResult<Span, Expr> {
+fn expr8(source: Span) -> IResult<Expr> {
     let (source, init) = expr7(source)?;
     fold_many0(preceded(s(tag("&&")), s(expr7)), init, Expr::logical_and)(source)
 }
 
-fn expr7(source: Span) -> IResult<Span, Expr> {
+fn expr7(source: Span) -> IResult<Expr> {
     let (source, init) = expr6(source)?;
     let cmp = alt((
         tag(">="),
@@ -167,28 +167,28 @@ fn expr7(source: Span) -> IResult<Span, Expr> {
     })(source)
 }
 
-fn expr6(source: Span) -> IResult<Span, Expr> {
+fn expr6(source: Span) -> IResult<Expr> {
     let (source, init) = expr5(source)?;
     fold_many0(pair(s(tag("|")), s(expr5)), init, |lhs, (op, rhs)| {
         Expr::binary_op(op.fragment(), lhs, rhs)
     })(source)
 }
 
-fn expr5(source: Span) -> IResult<Span, Expr> {
+fn expr5(source: Span) -> IResult<Expr> {
     let (source, init) = expr4(source)?;
     fold_many0(pair(s(tag("^")), s(expr4)), init, |lhs, (op, rhs)| {
         Expr::binary_op(op.fragment(), lhs, rhs)
     })(source)
 }
 
-fn expr4(source: Span) -> IResult<Span, Expr> {
+fn expr4(source: Span) -> IResult<Expr> {
     let (source, init) = expr3(source)?;
     fold_many0(pair(s(tag("&")), s(expr3)), init, |lhs, (op, rhs)| {
         Expr::binary_op(&op.to_string(), lhs, rhs)
     })(source)
 }
 
-fn expr3(source: Span) -> IResult<Span, Expr> {
+fn expr3(source: Span) -> IResult<Expr> {
     let (source, init) = expr2(source)?;
     fold_many0(
         pair(s(alt((tag(">>"), tag("<<")))), s(expr2)),
@@ -197,25 +197,25 @@ fn expr3(source: Span) -> IResult<Span, Expr> {
     )(source)
 }
 
-fn expr2(source: Span) -> IResult<Span, Expr> {
+fn expr2(source: Span) -> IResult<Expr> {
     let (source, init) = expr1(source)?;
     fold_many0(pair(s(one_of("+-")), s(expr1)), init, |lhs, (op, rhs)| {
         Expr::binary_op(&op.to_string(), lhs, rhs)
     })(source)
 }
 
-fn expr1(source: Span) -> IResult<Span, Expr> {
+fn expr1(source: Span) -> IResult<Expr> {
     let (source, init) = term(source)?;
     fold_many0(pair(s(one_of("*/%")), s(term)), init, |lhs, (op, rhs)| {
         Expr::binary_op(&op.to_string(), lhs, rhs)
     })(source)
 }
 
-fn term(source: Span) -> IResult<Span, Expr> {
+fn term(source: Span) -> IResult<Expr> {
     primary(source)
 }
 
-fn primary(source: Span) -> IResult<Span, Expr> {
+fn primary(source: Span) -> IResult<Expr> {
     alt((
         map(identifier, Expr::identifier),
         map(literal, Expr::Literal),
@@ -223,25 +223,25 @@ fn primary(source: Span) -> IResult<Span, Expr> {
     ))(source)
 }
 
-fn identifier(source: Span) -> IResult<Span, Span> {
+fn identifier(source: Span) -> IResult<Span> {
     recognize(pair(
         alt((alpha1, tag("_"))),
         many0(alt((alphanumeric1, tag("_")))),
     ))(source)
 }
 
-fn literal(source: Span) -> IResult<Span, Literal> {
+fn literal(source: Span) -> IResult<Literal> {
     alt((integer, character))(source)
 }
 
-fn integer(source: Span) -> IResult<Span, Literal> {
+fn integer(source: Span) -> IResult<Literal> {
     let (source, position) = position(source)?;
     let (source, val) = digit1(source)?;
     let val = val.parse().unwrap();
     Ok((source, Literal::Integer { position, val }))
 }
 
-fn character(source: Span) -> IResult<Span, Literal> {
+fn character(source: Span) -> IResult<Literal> {
     let simple_char = map(satisfy(|c| c != '\\' && c.is_ascii()), |ch| ch as u8);
     let escaped_char = preceded(
         char('\\'),
@@ -265,20 +265,20 @@ fn character(source: Span) -> IResult<Span, Literal> {
     Ok((source, Literal::Character { position, val }))
 }
 
-fn s<'a, O, E: ParseError<Span<'a>>>(
-    f: impl Parser<Span<'a>, O, E>,
-) -> impl FnMut(Span<'a>) -> IResult<Span, O, E> {
+fn s<'a, O>(
+    f: impl Parser<Span<'a>, O, nom::error::Error<Span<'a>>>,
+) -> impl FnMut(Span<'a>) -> IResult<O> {
     preceded(skip, f)
 }
 
-fn skip<'a, E: ParseError<Span<'a>>>(source: Span<'a>) -> IResult<Span, Span, E> {
+fn skip(source: Span) -> IResult<Span> {
     use nom::InputTakeAtPosition as _;
     source.split_at_position_complete(|item| !item.is_ascii_whitespace())
 }
 
-fn paren<'a, O, E: ParseError<Span<'a>>>(
-    f: impl Parser<Span<'a>, O, E>,
-) -> impl FnMut(Span<'a>) -> IResult<Span, O, E> {
+fn paren<'a, O>(
+    f: impl Parser<Span<'a>, O, nom::error::Error<Span<'a>>>,
+) -> impl FnMut(Span<'a>) -> IResult<O> {
     delimited(tag("("), s(f), s(tag(")")))
 }
 
